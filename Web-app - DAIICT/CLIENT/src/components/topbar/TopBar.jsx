52c705994400736577e7e1f979d0ca4ffbf14726
Vraj Shah <svraj073@gmail.com>
2022-10-13 21:45:33 +0530

    Add files via upload

---
+++ Web-app - DAIICT/CLIENT/src/components/topbar/TopBar.jsx
@@ -0,0 +1,79 @@
+import { useContext } from "react";
+import { Link } from "react-router-dom";
+import { Context } from "../../context/Context";
+import "./topbar.css";
+
+export default function TopBar() {
+  const { user, dispatch } = useContext(Context);
+  const PF = "http://localhost:5000/images/"
+
+  const handleLogout = () => {
+    dispatch({ type: "LOGOUT" });
+  };
+  return (
+    <div className="top">
+      <div className="topLeft">
+        <i className="topIcon fab fa-github-square">
+<a href="https://github.com/Vraj1103/DA-IICT-Hackathon"  rel=""></a>
+        </i>
+        <i className="topIcon fab fa-twitter-square">
+          <a href=" https://twitter.com/shahsonic?t=fdVATO8hw7KyKRNUt6tURg&s=08"  rel=""></a>
+        </i>
+        <i className="topIcon fab fa-pinterest-square">
+          <a href="https://www.pinterest.com"></a>
+        </i>
+        <i className="topIcon fab fa-instagram-square">
+          <a href="https://www.instagram.com"></a>
+        </i>
+      </div>
+      <div className="topCenter">
+        <ul className="topList">
+          <li className="topListItem">
+            <Link className="link" to="/">
+              HOME
+            </Link>
+          </li>
+          <li className="topListItem">
+            <Link className="link" to="/">
+              ABOUT
+            </Link>
+          </li>
+          <li className="topListItem">
+            <Link className="link" to="/">
+              CONTACT
+            </Link>
+          </li>
+          <li className="topListItem">
+            <Link className="link" to="/write">
+              WRITE
+            </Link>
+          </li>
+          <li className="topListItem" onClick={handleLogout}>
+            {user && "LOGOUT"}
+          </li>
+        </ul>
+      </div>
+      <div className="topRight">
+        {user ? (
+          <Link to="/settings">
+            <img className="topImg" src={PF+user.profilePic} alt="Profile" />
+          </Link>
+        ) : (
+          <ul className="topList">
+            <li className="topListItem">
+              <Link className="link" to="/login">
+                LOGIN
+              </Link>
+            </li>
+            <li className="topListItem">
+              <Link className="link" to="/register">
+                REGISTER
+              </Link>
+            </li>
+          </ul>
+        )}
+        <i className="topSearchIcon fas fa-search"></i>
+      </div>
+    </div>
+  );
+}
---
+++ Web-app - DAIICT/CLIENT/src/pages/about/About.jsx
@@ -0,0 +1,11 @@
+import  "./about.css";
+
+export default function about() {
+  return (
+    <div    className="aboutus">
+        <h1 className="aboutusText">aboutus</h1>
+        <img className="aboutusImg" src="./Our team.png" alt="" />
+<img src="./Team Introduction Instagram Post.png" alt="" />
+    </div>
+  )
+}
